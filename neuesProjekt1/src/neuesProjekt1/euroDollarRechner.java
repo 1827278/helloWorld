61e57cf587759f6d5bc6d4e96e8cc7a2c4ff84f3
1827278 <1827278@stud.hs-mannheim.de>
2018-10-16 12:40:17 +0200

    Neu

---
+++ neuesProjekt1/src/neuesProjekt1/euroDollarRechner.java
@@ -0,0 +1,51 @@
+package neuesProjekt1;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class euroDollarRechner {
+
+	public static void main(String[] args) throws IOException {
+		// Variablendeklaration
+		Scanner scan = new Scanner(System.in);
+		int Währung;
+		String Währung1;
+		float euro;
+		String euro1;
+		float dollar;
+		String dollar1;
+		boolean again=true;
+
+		
+	
+		// decide Euro or Dollar
+		System.out.println("Wählen Sie 1 für Euro in Dollar oder 2 für Dollar in Euro aus:");
+		Währung1 = scan.nextLine();
+		Währung = Integer.parseInt(Währung1);
+
+		// If Euro
+		if (Währung == 1) {
+			System.out.println("Geben Sie einen Betrag in Euro ein:");
+			euro1 = scan.nextLine();
+			euro = Float.parseFloat(euro1);
+			System.out.println(euro * 1.34);
+			System.out.println("Weiter Umrechnung benötigt? Ja/Nein");
+			
+		}
+		// If Dollar
+		if (Währung == 2) {
+			System.out.println("Geben Sie einen Betrag in Dollar ein:");
+			dollar1 = scan.nextLine();
+			dollar = Float.parseFloat(dollar1);
+			System.out.println(dollar / 1.34);
+			System.out.println("Weitere Umrechnung benötigt?");
+			
+		}
+		// Else Error
+		else {
+			System.out.println("Error");
+			 }
+	
+	
+	}
+}
